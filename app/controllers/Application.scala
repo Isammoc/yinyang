06ffc73ac7067865af43b5e31fbe4101c3ea364c
Isammoc OFF <isammoc@gmail.com>
2013-12-29 13:33:24 +0100

    Créer une partie

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,22 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+  var currentId = 0
+  def nextId = {
+    val ans = currentId
+    currentId += 1
+    ans
+  }
 
   def index = Action {
     Ok(views.html.index())
   }
 
+  def newGame = Action {
+    Redirect(routes.Application.game(nextId))
+  }
+
+  def game(id: Int) = Action {
+    Ok(views.html.game(id))
+  }
 }
