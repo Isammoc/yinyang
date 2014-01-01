03e84a3c65dc0d84d83457e41ce094d0f095a6a6
Isammoc OFF <isammoc@gmail.com>
2014-01-01 17:42:26 +0100

    Refactor de l'utilisation du plugin Redis

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,33 +1,39 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.Play.current
 import com.typesafe.plugin.RedisPlugin
 import org.sedis.Dress
+import play.api._
+import play.api.Play.current
+import play.api.mvc._
+import scala.concurrent.Future
+import redis.clients.jedis.Jedis
 
 object Application extends Controller {
 
-  def index = Action {
-    current.plugin[RedisPlugin].map ( _.sedisPool.withJedisClient { client =>
-      import Dress._
-      client.setnx("game:next.id", "0")
-      val current = client.get("game:next.id")
-      val waitingGames = List.range(1, current.toLong + 1)
-      Ok(views.html.index(waitingGames))
-    }).getOrElse(InternalServerError("Redis Play Plugin not here"))
+  def index = JedisAction { request =>
+    request.jedis.setnx("game:next.id", "0")
+    val current = request.jedis.get("game:next.id")
+    val waitingGames = List.range(1, current.toLong + 1)
+    Ok(views.html.index(waitingGames))
   }
 
-  def newGame = Action {
-    current.plugin[RedisPlugin].map ( _.sedisPool.withJedisClient { client =>
-      import Dress._
-      client.setnx("game:next.id", "0")
-      val nextId = client.incr("game:next.id")
-      Redirect(routes.Application.game(nextId))
-    }).getOrElse(InternalServerError("Redis Play Plugin not here"))
+  def newGame = JedisAction { request =>
+    request.jedis.setnx("game:next.id", "0")
+    val nextId = request.jedis.incr("game:next.id")
+    Redirect(routes.Application.game(nextId))
   }
 
   def game(id: Long) = Action {
     Ok(views.html.game(id))
+  }
+
+  class JedisRequest[A](val jedis: Jedis, request: Request[A]) extends WrappedRequest[A](request)
+
+  object JedisAction extends ActionBuilder[JedisRequest] {
+    def invokeBlock[A](request: Request[A], block: (JedisRequest[A]) => Future[SimpleResult]) = {
+      current.plugin[RedisPlugin].map(_.sedisPool.withJedisClient { client =>
+        block(new JedisRequest(client, request))
+      }).getOrElse(Future.successful(InternalServerError("Redis Play Plugin not here")))
+    }
   }
 }
