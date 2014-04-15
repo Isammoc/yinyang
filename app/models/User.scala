a3cd48b00d167650201dd474b64fba549eff7231
Isammoc OFF <isammoc@gmail.com>
2014-04-15 14:59:54 +0200

    #3 : User can change her nickname

--- app/models/User.scala
+++ app/models/User.scala
@@ -2,7 +2,11 @@ package models
 
 abstract class User {
   val id: Long
+  val isGuest = false
 }
 
 case class AnonymousUser(id:Long) extends User
 
+case class GuestUser(id: Long, nickname: String) extends User {
+  override val isGuest = true
+}
